52c11ec6c0d6ccb832de7123055a3c52e6f59752
Anika Anne <anika4dev@gmail.com>
2025-05-12 19:59:31 -0400

    remove firebase code

---
+++ src/server/auth.ts
@@ -0,0 +1,37 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [DiscordProvider, GoogleProvider],
+  // callbacks: {
+  //   session: ({ session, token }) => ({
+  //     ...session,
+  //     user: {
+  //       ...session.user,
+  //       id: token.sub,
+  //     },
+  //   }),
+  // },
+});
